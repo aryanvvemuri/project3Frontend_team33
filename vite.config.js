12b7f8455d46e2fbd21ce982d1997326d413d13f
aryanvvemuri <aryanvemuri@gmail.com>
2025-03-27 13:16:30 -0500

    render fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001 
-  }
+    host: '0.0.0.0', // Allow external access
+    port: process.env.PORT || 5173, // Use Render's assigned port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
 });
