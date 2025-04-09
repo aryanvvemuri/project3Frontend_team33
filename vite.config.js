45ad9dbe078067516a0e058255a004ccc2c17ca3
aryanvvemuri <aryanvemuri@gmail.com>
2025-04-09 16:46:49 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     host: '0.0.0.0', // Allow external access
     port: process.env.PORT || 5173, // Use Render's assigned port
     strictPort: true, // Ensure the port is strictly used
-    allowedHosts: ['frontend33-v41s.onrender.com'], // ✅ Add your Render host here
+    allowedHosts: ['project3frontend-team33.onrender.com'], // ✅ Add your Render host here
   },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['frontend33-v41s.onrender.com'],
+    allowedHosts: ['project3frontend-team33.onrender.com'],
   },
 });
